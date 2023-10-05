e45d5d94f38392b8c89e9cc78efd4eaf993d25c6
Yamin <myamin335@gmail.com>
2023-10-05 18:56:15 +0200

    HTTPError added

--- WorldOfPAYBACK/Core/Networking/HTTPError.swift
+++ WorldOfPAYBACK/Core/Networking/HTTPError.swift
@@ -1,5 +1,5 @@
 //
-//  HttpError.swift
+//  HTTPError.swift
 //  WorldOfPAYBACK
 //
 //  Created by Md. Yamin on 27.09.23.
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public enum HttpError: Swift.Error, Equatable {
+public enum HTTPError: Swift.Error, Equatable {
     case invalidRequestUrl
     case invalidResponse
     case invalidStatusCode(statusCode: Int)
