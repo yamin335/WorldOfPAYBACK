444e31e92fe9fe0ad4acfe0698c42c85057dced2
Yamin <myamin335@gmail.com>
2024-01-01 23:05:25 +0100

    "TransactionApiFactory" created

--- WorldOfPAYBACK/Data/ApiEndpoint/TransactionApiFactory.swift
+++ WorldOfPAYBACK/Data/ApiEndpoint/TransactionApiFactory.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+protocol TransactionApiFactoryType {
+    func createSuccessResponse() -> URLResource<TransactionListDTO>
+    func createErrorResponse() -> URLResource<TransactionListDTO>
+}
+
+struct TransactionApiFactory: TransactionApiFactoryType {
+    func createSuccessResponse() -> URLResource<TransactionListDTO> {
+        create(isErrorState: false)
+    }
+    
+    func createErrorResponse() -> URLResource<TransactionListDTO> {
+        create(isErrorState: true)
+    }
+    
+    private func create(isErrorState: Bool) -> URLResource<TransactionListDTO> {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        
+        let mockResult: MockURLResource.MockURLResourceResult =
+        isErrorState ? .failure(.invalidHttpUrlResponse) :.success(.file(jsonName: "PBTransactions"))
+        
+        return .get(
+            path: "/transactions",
+            mock: .init(
+                result: mockResult
+            ),
+            decoder: decoder
+        )
+    }
+}
