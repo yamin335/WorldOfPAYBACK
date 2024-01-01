1f37750dac10e365c726c28ee9c834f5b8265be8
Yamin <myamin335@gmail.com>
2024-01-01 23:04:35 +0100

    Common UI design file added

--- WorldOfPAYBACK/Core/System/SystemUIDesign.swift
+++ WorldOfPAYBACK/Core/System/SystemUIDesign.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import UIKit
+
+struct SystemUIDesign {
+    private init() {}
+    @MainActor
+    static func apply() {
+        // correct the transparency bug for Tab bars
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+    }
+}
