195667c92468bf7bf4b7be3d7476d6ec20e5efa2
Yamin <myamin335@gmail.com>
2024-01-02 23:00:58 +0100

    App state modified

--- WorldOfPAYBACK/Global State/AppState.swift
+++ WorldOfPAYBACK/Global State/AppState.swift
@@ -8,60 +8,24 @@
 import SwiftUI
 import Combine
 
-class AppState: ObservableObject {
-    enum AppDeployment: String {
-        case production = "Production"
-        case qa         = "QA"
-    }
-    
-    #if DEBUG
-    @Published var deploymentBuild: AppDeployment = .qa
-    #else
-    @Published var deploymentBuild: AppDeployment = .production
-    #endif
+protocol AppStateType: ObservableObject {
+    var isLoggedIn: Bool { get set }
+    var isSplashShown: Bool { get set }
+}
+
+class AppState: AppStateType {
+    private let keyLoggedStatus = "key_login"
     
     @Published var isLoggedIn: Bool {
         didSet {
-            UserDefaults.standard.set(isLoggedIn, forKey: AppConstants.keyIsLoggedIn)
+            UserDefaults.standard.set(isLoggedIn, forKey: keyLoggedStatus)
         }
     }
-    
-    @Published var sumOfTransaction: Int
-    @Published var currency: String
     
     @Published var isSplashShown: Bool
-    
-    @Published var isLoading: Bool
-    
-    @Published var isConnected: Bool
-    
-    @Published var accessToken: String? {
-        didSet {
-            if accessToken == nil {
-                UserDefaults.standard.removeObject(forKey: "access_token")
-            } else {
-                UserDefaults.standard.set(accessToken, forKey: "access_token")
-            }
-        }
-    }
-    
-    @Published var isShowingErrorMsg: Bool = false
-    @Published var isShowingSuccessMsg: Bool = false
-    @Published var errorMessage: String = ""
-    @Published var successMessage = ""
-    
         
     init() {
-        self.sumOfTransaction = 0
-        self.currency = AppConstants.defaultCurrency
         self.isSplashShown = false
-        self.isLoading = false
-        self.accessToken = UserDefaults.standard.string(forKey: "access_token") ?? ""
-        self.isShowingErrorMsg = false
-        self.isShowingSuccessMsg = false
-        self.errorMessage = ""
-        self.successMessage = ""
         self.isLoggedIn = UserDefaults.standard.bool(forKey: AppConstants.keyIsLoggedIn)
-        self.isConnected = false
     }
 }
